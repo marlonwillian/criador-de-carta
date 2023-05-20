394b46de055dc794335de0447497cc1d2f4de64d
Marlon Willian Silva Barros <mwsb2004@gmail.com>
2023-05-19 23:27:40 -0300

    começando javascript

---
+++ geradordecarta/script.js
@@ -0,0 +1,47 @@
+// img // 
+
+let img_raridade = document.getElementById("iraridade")
+let img_pais = document.getElementById("ipais")
+let img_clube = document.getElementById("iclube")
+let img_jogador = document.getElementById("ijogador")
+let img_entrosamento = document.getElementById("ientrosamento")
+
+// geral, posição e nome //
+
+let geral = getElementById("geral")
+let posicao = getElementById("posicao")
+let nome = getElementById("nome")
+
+// stats - lado esquerdo //
+
+let ritmo = document.getElementById("srit")
+let finalizacao = document.getElementById("sfin")
+let passe = document.getElementById("spas")
+
+// stats - lado direito //
+
+let conducao = document.getElementById("scon")
+let defesa = document.getElementById("sdef")
+let fisico = document.getElementById("sfis")
+
+// informações personalizadas //
+
+let input_nome = document.getElementById("inpnome")
+
+// informações personalizadas - info 1 //
+
+let sel_clube = document.getElementById("selclube")
+let sel_pais = document.getElementById("selpais")
+let sel_posicao = document.getElementById("selposicao")
+
+// informações personalizadas - info 2 //
+
+let sel_raridade = document.getElementById("selraridade")
+let sel_jogador = document.getElementById("seljogador")
+let sel_entrosamento = document.getElementById("selentrosamento")
+
+// descrições //
+
+let des_raridade = document.getElementById("desraridade")
+let des_entrosamento = document.getElementById("desentrosamento")
+
